96a7679d2601ec6999ebd5cf56994cd8ef1c79e5
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-03 00:28:03 +0530

    First Commit.

---
+++ OnboardingApp/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  OnboardingApp
+//
+//  Created by Maulik Shah on 2/3/25.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
---
+++ OnboardingApp/OnboardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAppApp.swift
+//  OnboardingApp
+//
+//  Created by Maulik Shah on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
